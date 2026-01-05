708037dfc080a32aa9a4aa2b27b656d90200cd50
Jitendra Sahoo <sahooj168@gmail.com>
2026-01-05 23:44:28 +0530

    Implement Kafka Consumer

--- server.ts
+++ server.ts
@@ -1,18 +1,25 @@
-import app from "./src/app";
 import config from "config";
-import logger from "./src/config/logger";
+import app from "./src/app";
+import { createMessageBroker } from "./src/common/factories/brokerFactory";
 import connectDB from "./src/config/db";
+import logger from "./src/config/logger";
+import { MessageBroker } from "./src/types/broker";
 
 const startServer = async () => {
   const PORT = config.get("server.port") || 5503;
-
+  let broker: MessageBroker | null = null;
   try {
     await connectDB();
-
+    broker = createMessageBroker();
+    await broker.connectConsumer();
+    await broker.consumeMessage(['product'], false);
     app
       .listen(PORT, () => console.log(`Listening on port ${PORT}`))
-      .on("error", (err) => {
+      .on("error", async (err) => {
         console.log("err", err.message);
+        if (broker) {
+          await broker.disconnectConsumer();
+        }
         process.exit(1);
       });
   } catch (err) {
