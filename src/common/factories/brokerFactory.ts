48a0631b28a56a1430fcafc3fa47df609166a9c0
Jitendra Sahoo <sahooj168@gmail.com>
2026-01-25 02:15:59 +0530

    remove log of brokers

--- src/common/factories/brokerFactory.ts
+++ src/common/factories/brokerFactory.ts
@@ -5,7 +5,6 @@ import { MessageBroker } from "../../types/broker";
 let broker: MessageBroker | null = null;
 export const createMessageBroker = (): MessageBroker => {
   if (!broker) {
-    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥", config.get("kafka.brokers"));
     broker = new KafkaBroker("order-service", config.get("kafka.brokers"));
   }
   return broker;
