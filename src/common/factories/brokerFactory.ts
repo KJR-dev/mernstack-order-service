195bd87c6c41d879473cc9334dcdc7eb20897da6
Jitendra Sahoo <sahooj168@gmail.com>
2026-01-25 02:03:34 +0530

    log config.get("kafka.brokers")

--- src/common/factories/brokerFactory.ts
+++ src/common/factories/brokerFactory.ts
@@ -5,6 +5,7 @@ import { MessageBroker } from "../../types/broker";
 let broker: MessageBroker | null = null;
 export const createMessageBroker = (): MessageBroker => {
   if (!broker) {
+    console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥", config.get("kafka.brokers"));
     broker = new KafkaBroker("order-service", config.get("kafka.brokers"));
   }
   return broker;
